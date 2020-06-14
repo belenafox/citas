e6cac9d58f0f2f847175c12fb7bc595c04cfd776
belenafox <c.isla1171@gmail.com>
2020-06-13 21:05:28 -0400

    localStorage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import Formulario from './components/Formulario/Formulario';
 import Cita from './components/Formulario/Cita';
 
 function App() {
+  //LocalStorage solo almacena strings
+  //citas en localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {//si no existen citas, inicia con un arreglo vacio
+    citasIniciales = [];
+  }
 
   // Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
 
   // useEffect para realizar ciertas operaciones cuando el state cambia, se ejecuta cuando el componente esta listo y tbn cuando hay una cambio de estado
   useEffect(() => {
-    console.log('listo')
-  },[]);//para que se ejecute solo una vez, se pasa un arreglo vacio
+    if(citasIniciales) {
+      // si hay citas
+      localStorage.setItem('citas',JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  },[citas]);//para que se ejecute solo una vez, se pasa un arreglo vacio--cuando el state de citas cambie, se ejecutara el useEffect
 
   // Funcion que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
